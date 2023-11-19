ab567ff80e2100f78d1bb0800ccc55983fda773c
Kenneth Ossai <kenossai1@gmail.com>
2023-11-19 18:51:57 +0000

    Updated backend ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,158 +1,133 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-100">
 
-<!-- Mirrored from demo.codevibrant.com/html/kavya/index4.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 12 Nov 2023 13:33:02 GMT -->
+
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<meta name="robots" content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="She Elivate Blog" />
+	<meta property="og:title" content="She Elivate Blog" />
+	<meta property="og:description" content="She Elivate Blog" />
+	<meta property="og:image" content="social-image.png" />
+	<meta name="format-detection" content="telephone=no">
 
-  <!-- Favicon -->
-  <link rel="icon" type="image/png" sizes="48x48" href="assets/images/favicon.png">
+	<!-- PAGE TITLE HERE -->
+	<title>Login</title>
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700&amp;display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800&amp;display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Great+Vibes&amp;display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,400i,500,500i,600&amp;display=swap" rel="stylesheet">
+	<!-- FAVICONS ICON -->
+	<link rel="shortcut icon" type="image/png" href="images/favicon.png" />
+    <link href="{{ asset('backend/css/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('backend/css/toastr.css') }}" />
 
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="{{ asset('client/css/bootstrap.min.css') }}" />
-
-  <!-- Fontawesome CSS-->
-  <link rel="stylesheet" href="{{ asset('client/css/all.css') }}" />
-
-  <!-- slick css -->
-  <link rel="stylesheet" href="{{ asset('client/css/slick.css') }}">
-  <link rel="stylesheet" href="{{ asset('client/css/slick-theme.css') }}">
-
-  <!-- Custom CSS -->
-  <link rel="stylesheet" href="{{ asset('client/css/preloader.css') }}" />
-  <link rel="stylesheet" href="{{ asset('client/css/style.css') }}" />
-  <link rel="stylesheet" href="{{ asset('client/css/responsive.css') }}" />
-  <link rel="stylesheet" href="{{ asset('client/css/dark.css') }}" />
-  <link rel="stylesheet" href="{{ asset('client/css/toastr.css') }}" />
-
-  <title>Login</title>
 </head>
 
-<body>
-  <!-- Preloader -->
-  <div class="preloader-wrapper">
-    <div class="preloader">
-      <div class="preloader-circle" id="status">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </div>
-  </div>
-  <!-- Preloader end -->
-
-  <!-- page header end -->
-  <section class="contact-section" style="padding-top: 12rem;">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-5" style="margin: auto; text-align: center;">
-          <form class="contact-form" method="POST" action="{{ route('login') }}">
-            @csrf
-            <h3>Sign in</h3>
-            <p>Enter your log-in details for unlimited access to She Eliva8's content</p>
-
-            <div class="mb-3">
-                <input id="email" type="email" name="email" :value="old('email')" autofocus autocomplete="username" class="form-control" placeholder="Email or Username">
+<body class="vh-100">
+    <div class="authincation h-100" >
+        <div class="container h-100">
+            <div class="row justify-content-center h-100 align-items-center">
+                <div class="col-md-6">
+                    <div class="authincation-content">
+                        <div class="row no-gutters">
+                            <div class="col-xl-12">
+                                <div class="auth-form">
+									<div class="text-center mb-3">
+										<a href="index.html"><img src="images/logo-full.png" alt=""></a>
+									</div>
+                                    <h4 class="text-center mb-4">Sign in your account</h4>
+                                    <p class="text-center">Enter your log-in details for unlimited access to She Eliva8's content</p>
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <div class="mb-3">
+                                            <label class="mb-1"><strong>Email</strong></label>
+                                            <input id="email" type="email" name="email" :value="old('email')" autofocus autocomplete="username" class="form-control" placeholder="Email or Username">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="mb-1"><strong>Password</strong></label>
+                                            <input type="password" class="form-control" type="password" name="password" autocomplete="current-password" placeholder="Password">
+                                        </div>
+                                        <div class="row d-flex justify-content-between mt-4 mb-2">
+                                            <div class="mb-3">
+                                               <div class="form-check custom-checkbox ms-1">
+													<input type="checkbox" class="form-check-input" id="remember_me" name="remember">
+													<label class="form-check-label" for="basic_checkbox_1">Remember my preference</label>
+												</div>
+                                            </div>
+                                            <div class="mb-3">
+                                                @if (Route::has('password.request'))
+                                                    <a class="" href="{{ route('password.request') }}">
+                                                        {{ __('Forgot your password?') }}
+                                                    </a>
+                                                @endif
+                                            </div>
+                                        </div>
+                                        <div class="text-center">
+                                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                                        </div>
+                                    </form>
+                                    <div class="new-account mt-3">
+                                        <p>Don't have an account? <a class="text-primary" href="{{ route('register') }}">Sign up</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <div class="mb-3">
-                <input type="password" class="form-control" type="password" name="password" autocomplete="current-password" placeholder="Password">
-            </div>
-            <div class="form-switch text-left text-info">
-                @if (Route::has('password.request'))
-                      <a class="" href="{{ route('password.request') }}">
-                          {{ __('Forgot your password?') }}
-                      </a>
-                  @endif
-              </div>
-            <button type="submit" class="btn bg-gradient-info w-50 mt-4 mb-0">Login</button>
-          </form>
-          <div class=" text-center pt-5 px-lg-2 px-1">
-            <p class="mb-4 text-sm mx-auto">
-              Don't have an account?
-              <a href="javascript:;" class="text-info text-gradient font-weight-bold">Sign up</a>
-            </p>
-          </div>
         </div>
-      </div>
     </div>
-  </section>
-
-  <!-- Javascript -->
-  <script src="{{ asset('client/js/jquery-3.4.1.min.js') }}"></script>
-  <script src="{{ asset('client/js/bootstrap.min.js') }}"></script>
-  <script src="{{ asset('client/js/slick.min.js') }}"></script>
-  <script src="{{ asset('client/js/jquery.sticky.js') }}"></script>
-  <script src="{{ asset('client/js/ResizeSensor.min.js') }}"></script>
-  <script src="{{ asset('client/js/theia-sticky-sidebar.min.js') }}"></script>
-  <script src="{{ asset('client/js/main.js') }}"></script>
-  <script src="{{ asset('client/js/toastr.js') }}"></script>
-  <script>
-
-    // success message popup notification
-
-    @if(Session::has('success'))
-
-        toastr.success("{{ Session::get('success') }}");
-
-    @endif
 
 
-    // info message popup notification
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <!-- Required vendors -->
+    <script src="{{ asset('backend/vendor/global/global.min.js') }}"></script>
+    <script src="{{ asset('backend/js/custom.min.js') }}"></script>
+    <script src="{{ asset('backend/js/deznav-init.js') }}"></script>
+	{{-- <script src="{{ asset('backend/js/styleSwitcher.js') }}"></script> --}}
+    <script src="{{ asset('backend/js/toastr.js') }}"></script>
+    <script>
 
-    @if(Session::has('info'))
+        // success message popup notification
 
-        toastr.info("{{ Session::get('info') }}");
+        @if(Session::has('success'))
 
-    @endif
+            toastr.success("{{ Session::get('success') }}");
 
-
-    // warning message popup notification
-
-    @if(Session::has('warning'))
-
-        toastr.warning("{{ Session::get('warning') }}");
-
-    @endif
-
-
-    // error message popup notification
-
-    @if(Session::has('error'))
-        toastr.error("{{ Session::get('error') }}");
-    @endif
-
-    @foreach($errors->all() as $error)
-        toastr.error("{{ $error }}")
-    @endforeach
+        @endif
 
 
+        // info message popup notification
 
-</script>
-  <style>
-    .bg-gradient-info {
-        background-color: black;
-        color: white;
-        border: none;
-    }
-    .bg-gradient-info:hover {
-        color: white;
-        border: none;
-    }
-    .form-input{
-        height: 50px !important;
-    }
-  </style>
+        @if(Session::has('info'))
+
+            toastr.info("{{ Session::get('info') }}");
+
+        @endif
+
+
+        // warning message popup notification
+
+        @if(Session::has('warning'))
+
+            toastr.warning("{{ Session::get('warning') }}");
+
+        @endif
+
+
+        // error message popup notification
+
+        @if(Session::has('error'))
+            toastr.error("{{ Session::get('error') }}");
+        @endif
+
+        @foreach($errors->all() as $error)
+            toastr.error("{{ $error }}")
+        @endforeach
+    </script>
 </body>
-
-
 </html>
