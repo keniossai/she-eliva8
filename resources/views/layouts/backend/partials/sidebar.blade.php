7a3015e4ace4b64aff0b9e85a8c6b3bd81551add
Kenneth Ossai <kenossai1@gmail.com>
2023-11-19 22:43:10 +0000

    Update sidebar.blade.php

--- resources/views/layouts/backend/partials/sidebar.blade.php
+++ resources/views/layouts/backend/partials/sidebar.blade.php
@@ -1,20 +1,11 @@
 <div class="deznav">
     <div class="deznav-scroll">
         <ul class="metismenu" id="menu">
-            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+            <li>
+                <a class="has-arrow ai-icon">
                     <i class="flaticon-025-dashboard"></i>
                     <span class="nav-text">Dashboard</span>
                 </a>
-                <ul aria-expanded="false">
-                    <li><a href="index.html">Dashboard Light</a></li>
-                    <li><a href="index-2.html">Dashboard Dark</a></li>
-                    <li><a href="order-page-list.html">Orders</a></li>
-                    <li><a href="order-details-page.html">Order Details</a></li>
-                    <li><a href="customer-page-list.html">Customers</a></li>
-                    <li><a href="analytics.html">Analytics</a></li>
-                    <li><a href="review.html">Review</a></li>
-                </ul>
-
             </li>
             <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
                 <i class="flaticon-050-info"></i>
