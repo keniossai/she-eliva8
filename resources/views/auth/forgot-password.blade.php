54306b90f62cf1335694afe4d7dda45f51b79336
Kenneth Ossai <kenossai1@gmail.com>
2023-11-19 19:52:23 +0000

    Updated password-reset ui

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,114 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+
+
+<head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<meta name="robots" content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="She Elivate Blog" />
+	<meta property="og:title" content="She Elivate Blog" />
+	<meta property="og:description" content="She Elivate Blog" />
+	<meta property="og:image" content="social-image.png" />
+	<meta name="format-detection" content="telephone=no">
+
+	<!-- PAGE TITLE HERE -->
+	<title>Register</title>
+
+	<!-- FAVICONS ICON -->
+	<link rel="shortcut icon" type="image/png" href="images/favicon.png" />
+    <link href="{{ asset('backend/css/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('backend/css/toastr.css') }}" />
+
+</head>
+
+<body class="vh-100">
+    <div class="authincation h-100">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100 align-items-center">
+                <div class="col-md-6">
+                    <div class="authincation-content">
+                        <div class="row no-gutters">
+                            <div class="col-xl-12">
+                                <div class="auth-form">
+									<div class="text-center mb-3">
+										<a href="index.html"><img src="images/logo-full.png" alt=""></a>
+									</div>
+                                    <h4 class="text-center mb-4">Forgot Password</h4>
+                                    <div class="mb-4 text-sm text-gray-600">
+                                        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+                                    </div>
+                                    <form method="POST" action="{{ route('password.email') }}">
+                                        @csrf
+                                        <div class="mb-3">
+                                            <label><strong>Email</strong></label>
+                                            <input class="form-control" id="email" type="email" name="email" :value="old('email')" autofocus>
+                                        </div>
+                                        <div class="text-center">
+                                            <button type="submit" class="btn btn-primary btn-block">{{ __('Email Password Reset Link') }}</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <!-- Required vendors -->
+    <script src="{{ asset('backend/vendor/global/global.min.js') }}"></script>
+    <script src="{{ asset('backend/js/custom.min.js') }}"></script>
+    <script src="{{ asset('backend/js/deznav-init.js') }}"></script>
+	{{-- <script src="{{ asset('backend/js/styleSwitcher.js') }}"></script> --}}
+    <script src="{{ asset('backend/js/toastr.js') }}"></script>
+    <script>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+        // success message popup notification
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+        @if(Session::has('success'))
+
+            toastr.success("{{ Session::get('success') }}");
+
+        @endif
+
+
+        // info message popup notification
+
+        @if(Session::has('info'))
+
+            toastr.info("{{ Session::get('info') }}");
+
+        @endif
+
+
+        // warning message popup notification
+
+        @if(Session::has('warning'))
+
+            toastr.warning("{{ Session::get('warning') }}");
+
+        @endif
+
+
+        // error message popup notification
+
+        @if(Session::has('error'))
+            toastr.error("{{ Session::get('error') }}");
+        @endif
+
+        @foreach($errors->all() as $error)
+            toastr.error("{{ $error }}")
+        @endforeach
+    </script>
+</body>
+</html>
+
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,125 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<!DOCTYPE html>
+<html lang="en" class="h-100">
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+
+<head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<meta name="robots" content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="She Elivate Blog" />
+	<meta property="og:title" content="She Elivate Blog" />
+	<meta property="og:description" content="She Elivate Blog" />
+	<meta property="og:image" content="social-image.png" />
+	<meta name="format-detection" content="telephone=no">
+
+	<!-- PAGE TITLE HERE -->
+	<title>Register</title>
+
+	<!-- FAVICONS ICON -->
+	<link rel="shortcut icon" type="image/png" href="images/favicon.png" />
+    <link href="{{ asset('backend/css/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('backend/css/toastr.css') }}" />
+
+</head>
+
+<body class="vh-100">
+    <div class="authincation h-100">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100 align-items-center">
+                <div class="col-md-6">
+                    <div class="authincation-content">
+                        <div class="row no-gutters">
+                            <div class="col-xl-12">
+                                <div class="auth-form">
+									<div class="text-center mb-3">
+										<a href="index.html"><img src="images/logo-full.png" alt=""></a>
+									</div>
+                                    <h4 class="text-center mb-4">Sign up your account</h4>
+                                    <form method="POST" action="{{ route('register') }}">
+                                        @csrf
+                                        <div class="mb-3">
+                                            <label class="mb-1"><strong>Name</strong></label>
+                                            <input type="text" class="form-control" name="name" id="name" :value="old('name')"  autofocus autocomplete="name">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="mb-1"><strong>Email</strong></label>
+                                            <input class="form-control" id="email" type="email" name="email" :value="old('email')"  autocomplete="username">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="mb-1"><strong>Password</strong></label>
+                                            <input class="form-control" id="password" type="password" name="password"  autocomplete="new-password">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="mb-1"><strong>Password</strong></label>
+                                            <input class="form-control" id="password_confirmation" type="password" name="password_confirmation"  autocomplete="new-password">
+                                        </div>
+                                        <div class="text-center mt-4">
+                                            <button type="submit" class="btn btn-primary btn-block">{{ __('Register') }}</button>
+                                        </div>
+                                    </form>
+                                    <div class="new-account mt-3">
+                                        <p>Already have an account? <a class="text-primary" href="{{ route('login') }}">Sign in</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <!-- Required vendors -->
+    <script src="{{ asset('backend/vendor/global/global.min.js') }}"></script>
+    <script src="{{ asset('backend/js/custom.min.js') }}"></script>
+    <script src="{{ asset('backend/js/deznav-init.js') }}"></script>
+	{{-- <script src="{{ asset('backend/js/styleSwitcher.js') }}"></script> --}}
+    <script src="{{ asset('backend/js/toastr.js') }}"></script>
+    <script>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+        // success message popup notification
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+        @if(Session::has('success'))
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            toastr.success("{{ Session::get('success') }}");
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+        @endif
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
+        // info message popup notification
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+        @if(Session::has('info'))
+
+            toastr.info("{{ Session::get('info') }}");
+
+        @endif
+
+
+        // warning message popup notification
+
+        @if(Session::has('warning'))
+
+            toastr.warning("{{ Session::get('warning') }}");
+
+        @endif
+
+
+        // error message popup notification
+
+        @if(Session::has('error'))
+            toastr.error("{{ Session::get('error') }}");
+        @endif
+
+        @foreach($errors->all() as $error)
+            toastr.error("{{ $error }}")
+        @endforeach
+    </script>
+</body>
+</html>
