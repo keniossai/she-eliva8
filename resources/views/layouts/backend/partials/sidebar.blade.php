9c3cd1230f671d90e5daebd6ad167e42c0399934
Kenneth Ossai <kenossai1@gmail.com>
2023-11-20 18:05:27 +0000

    Update sidebar.blade.php

--- resources/views/layouts/backend/partials/sidebar.blade.php
+++ resources/views/layouts/backend/partials/sidebar.blade.php
@@ -39,11 +39,6 @@
                 <ul aria-expanded="false">
                     <li><a href="uc-select2.html">Select 2</a></li>
                     <li><a href="uc-nestable.html">Nestedable</a></li>
-                    <li><a href="uc-noui-slider.html">Noui Slider</a></li>
-                    <li><a href="uc-sweetalert.html">Sweet Alert</a></li>
-                    <li><a href="uc-toastr.html">Toastr</a></li>
-                    <li><a href="map-jqvmap.html">Jqv Map</a></li>
-                    <li><a href="uc-lightgallery.html">Light Gallery</a></li>
                 </ul>
             </li>
             <li><a href="widget-basic.html" class="ai-icon" aria-expanded="false">
