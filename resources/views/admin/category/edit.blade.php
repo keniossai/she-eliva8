98e1d4478a7e9852dbdcb9f67a2ed599d672689d
Kenneth Ossai <kenossai1@gmail.com>
2023-12-09 21:54:29 +0000

    Update edit.blade.php

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -10,7 +10,7 @@
     <div class="row">
         <div class="col-12">
             <div class="page-title-box d-flex align-items-center justify-content-between">
-                <h4 class="mb-0">Create Tag</h4>
+                <h4 class="mb-0">Edit Category</h4>
             </div>
         </div>
     </div>
@@ -23,27 +23,32 @@
                     <div class="p-4">
                         <div class="d-flex align-items-center">
                             <div class="flex-grow-1 overflow-hidden">
-                                <h5 class="font-size-16 mb-1">{{ $tag->name  }}</h5>
+                                <h5 class="font-size-16 mb-1">Edit: {{ $category->name  }}</h5>
                             </div>
                         </div>
                     </div>
                     <div class="p-4 border-top">
-                        <form method="POST" action="{{ route('tag.update',$tag->id) }}">
+                        <form method="POST" action="{{ route('category.update',$category->id) }}" enctype="multipart/form-data">
                             @csrf
                             @method('PUT')
                             <div class="row">
                                 <div class="col-sm-6">
                                     <div class="mb-3">
                                         <label class="form-label" for="metatitle">Name</label>
-                                        <input id="name" name="name" type="text" class="form-control" value="{{ $tag->name }}">
+                                        <input id="name" name="name" type="text" class="form-control" value="{{ $category->name }}">
                                     </div>
-
                                 </div>
 
                                 <div class="col-sm-6">
                                     <div class="mb-3">
                                         <label class="form-label" for="metakeywords">Slug</label>
-                                        <input id="slug" name="slug" type="text" class="form-control" placeholder="Optional">
+                                        <input id="slug" name="slug" type="text" class="form-control" placeholder="Optional" value="{{ $category->slug }}">
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="mb-3">
+                                        <label class="form-label" for="metatitle">Image</label>
+                                        <input id="image" name="image" type="file" class="form-control">
                                     </div>
                                 </div>
                             </div>
@@ -51,7 +56,7 @@
                                 <div class="col ms-auto">
                                     <div class="d-flex flex-reverse flex-wrap gap-2">
                                         <a href="{{ route('tag.index') }}" class="btn btn-danger"> <i class="uil uil-times"></i> Cancel </a>
-                                        <button type="submit" class="btn btn-success"> <i class="uil uil-file-alt"></i> Save </button>
+                                        <button type="submit" class="btn btn-success"> <i class="uil uil-file-alt"></i> Update </button>
                                     </div>
                                 </div> <!-- end col -->
                             </div> <!-- end row-->
