76bbd11c454c0744f9c3181841bd3485bc5a3379
Kenneth Ossai <kenossai1@gmail.com>
2023-12-09 14:07:20 +0000

    Create edit.blade.php

---
+++ resources/views/admin/tag/edit.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.backend.app')
+
+@section('title', 'Edit')
+
+
+@section('content')
+<div class="container-fluid">
+
+    <!-- start page title -->
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box d-flex align-items-center justify-content-between">
+                <h4 class="mb-0">Create Tag</h4>
+            </div>
+        </div>
+    </div>
+    <!-- end page title -->
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div id="addproduct-accordion" class="custom-accordion">
+                <div class="card">
+                    <div class="p-4">
+                        <div class="d-flex align-items-center">
+                            <div class="flex-grow-1 overflow-hidden">
+                                <h5 class="font-size-16 mb-1">{{ $tag->name  }}</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-4 border-top">
+                        <form method="POST" action="{{ route('tag.update',$tag->id) }}">
+                            @csrf
+                            @method('PUT')
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="mb-3">
+                                        <label class="form-label" for="metatitle">Name</label>
+                                        <input id="name" name="name" type="text" class="form-control" value="{{ $tag->name }}">
+                                    </div>
+
+                                </div>
+
+                                <div class="col-sm-6">
+                                    <div class="mb-3">
+                                        <label class="form-label" for="metakeywords">Slug</label>
+                                        <input id="slug" name="slug" type="text" class="form-control" placeholder="Optional">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mb-4">
+                                <div class="col ms-auto">
+                                    <div class="d-flex flex-reverse flex-wrap gap-2">
+                                        <a href="{{ route('tag.index') }}" class="btn btn-danger"> <i class="uil uil-times"></i> Cancel </a>
+                                        <button type="submit" class="btn btn-success"> <i class="uil uil-file-alt"></i> Save </button>
+                                    </div>
+                                </div> <!-- end col -->
+                            </div> <!-- end row-->
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end row -->
+
+
+
+</div>
+@endsection
