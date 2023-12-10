7f0ae1684f140ecd35cab9184db074f837e9957a
Kenneth Ossai <kenossai1@gmail.com>
2023-12-10 00:01:02 +0000

    Update index.blade.php

--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -1,18 +1,10 @@
 @extends('layouts.backend.app')
 
-@section('title','Category')
+@section('title','Post')
 
 @section('content')
 
 <div class="container-fluid">
-    <!-- start page title -->
-    <div class="row">
-        <div class="col-12">
-            <div class="page-title-box d-flex align-items-center justify-content-between">
-                <h4 class="mb-0">All @yield('title')</h4>
-            </div>
-        </div>
-    </div>
 
     <div class="row">
         <div class="col-lg-12">
@@ -20,59 +12,67 @@
                 <div class="card-body">
                     <div>
                         <div>
-                            <a href="{{ route('category.create') }}" class="btn btn-success waves-effect waves-light mb-3"><i class="fa fa-plus-circle" aria-hidden="true"></i> Add New Category</a>
+                            <a href="{{ route('post.create') }}" class="btn btn-success waves-effect waves-light mb-3"><i class="fa fa-plus-circle" aria-hidden="true"></i> Add Post</a>
                         </div>
 
                         <div class="table-responsive mb-4">
                             <table class="table table-centered datatable dt-responsive nowrap table-card-list" style="border-collapse: collapse; width: 100%;">
                                 <thead>
                                     <tr>
-                                        <th style="width: 20px;">
-                                            <div class="form-check text-center">
-                                                <input type="checkbox" class="form-check-input" id="customercheck">
-                                                <label class="form-check-label" for="customercheck"></label>
-                                            </div>
-                                        </th>
                                         <th>ID</th>
-                                        <th>Name</th>
-                                        <th>Slug</th>
+                                        <th>Title</th>
+                                        <th>Author</th>
+                                        <th>Visibility</th>
+                                        <th>Is Approved</th>
+                                        <th>Status</th>
                                         <th>Created At</th>
                                         <th>Updated At</th>
-                                        <th style="width: 120px;">Action</th>
+                                        <th >Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @foreach ($categories as $key=>$category)
-                                        <tr>
-                                            <td>
-                                                <div class="form-check text-center">
-                                                    <input type="checkbox" class="form-check-input" id="customercheck1">
-                                                    <label class="form-check-label" for="customercheck1"></label>
-                                                </div>
-                                            </td>
+                                    @foreach ($posts as $key=>$post)
+                                    <tr>
 
-                                            <td><a href="javascript: void(0);" class="text-reset  fw-bold">{{ $key + 1 }}</a> </td>
-                                            <td>
-                                                <span>{{ $category->name }}</span>
-                                            </td>
-                                            <td>{{ $category->slug }}</td>
-
-                                            <td>
-                                                {{ $category->created_at }}
-                                            </td>
-                                            <td>
-                                                <div class="badge bg-pill bg-success-subtle text-success font-size-12">Active</div>
-                                            </td>
-                                            <td>
-                                                <a href="{{ route('category.edit',$category->id) }}" class="px-1 text-primary"><i class="uil uil-pen font-size-18"></i></a>
-                                                <button type="button" style="border: none; background: transparent;" onclick="deleteCategory({{ $category->id }})" class="px-1 text-danger"><i class="uil uil-trash-alt font-size-18"></i></button>
-                                                <form action="{{ route('category.destroy',$category->id) }}" method="POST" id="delete-form-{{ $category->id }}" style="display: none;">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                </form>
-                                            </td>
-                                        </tr>
-
+                                        <td><a href="javascript: void(0);" class="text-reset  fw-bold">{{ $key + 1 }}</a> </td>
+                                        <td>
+                                            <span>{{ $post->title }}</span>
+                                        </td>
+                                        <td>
+                                            <span>{{ $post->user->name }}</span>
+                                        </td>
+                                        <td>
+                                            {{ $post->view_count}}
+                                        </td>
+                                        <td>
+                                            @if($post->is_approved == true)
+                                                <div class="badge bg-pill bg-success-subtle text-success font-size-12 px-2">Approved</div>
+                                            @else
+                                                <div class="badge bg-pill bg-warning-subtle text-warning font-size-12 px-2">Pending</div>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if($post->status == true)
+                                                <div class="badge bg-pill bg-success-subtle text-success font-size-12 px-2">Published</div>
+                                            @else
+                                                <div class="badge bg-pill bg-warning-subtle text-warning font-size-12 px-2">Pending</div>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            {{ $post->created_at }}
+                                        </td>
+                                        <td>
+                                            {{ $post->updated_at }}
+                                        </td>
+                                        <td>
+                                            <a href="{{ route('post.edit',$post->id) }}" class="px-1 text-primary"><i class="uil uil-pen font-size-18"></i></a>
+                                            <button type="button" style="border: none; background: transparent;" onclick="deletePost({{ $post->id }})" class="px-1 text-danger"><i class="uil uil-trash-alt font-size-18"></i></button>
+                                            <form action="{{ route('post.destroy',$post->id) }}" method="POST" id="delete-form-{{ $post->id }}" style="display: none;">
+                                                @csrf
+                                                @method('DELETE')
+                                            </form>
+                                        </td>
+                                    </tr>
                                     @endforeach
                                 </tbody>
                             </table>
@@ -87,7 +87,7 @@
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script type="text/javascript">
-    function deleteCategory(id){
+    function deletePost(id){
             Swal.fire({
             title: "Are you sure?",
             text: "You won't be able to revert this!",
