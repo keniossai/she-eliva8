c7a69cc0a5b5f81a53ec39eeba480e12dd94f3b9
Kenneth Ossai <kenossai1@gmail.com>
2023-12-10 00:00:59 +0000

    Update PostController.php

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -13,7 +13,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::latest()->get();
+        return view('admin.post.index', compact('posts'));
     }
 
     /**
