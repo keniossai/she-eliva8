14b377e3305465a5019b38148e4266ad477f4b06
Kenneth Ossai <kenossai1@gmail.com>
2023-12-10 21:29:45 +0000

    Update PostController.php

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Tag;
 use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -22,7 +27,9 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::all();
+        $tags = Tag::all();
+        return view('admin.post.create', compact('tags', 'categories'));
     }
 
     /**
@@ -30,7 +37,38 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request,[
+            'title' => 'required',
+            'image' => 'required',
+            'categories' => 'required',
+            'tags' => 'required',
+            'body' => 'required'
+        ]);
+
+        // Upload Image
+        $imageName = time().'.'.$request->image->extension();
+        $request->image->move(public_path('storage/post'),$imageName);
+
+        $post = new Post();
+        $post->user_id = Auth::id();
+        $post->title = $request->title;
+        $post->slug = Str::slug($request->title);
+        $post->image = $imageName;
+        $post->body = $request->body;
+        if(isset($request->status))
+        {
+            $post->status = true;
+        }else {
+            $post->status = false;
+        }
+        $post->is_approved = true;
+
+        $post->save();
+
+        $post->categories()->attach($request->categories);
+        $post->tags()->attach($request->tags);
+
+        return redirect()->route('post.index')->with('success', 'Post Added Successfully');
     }
 
     /**
@@ -46,7 +84,9 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        $categories = Category::all();
+        $tags = Tag::all();
+        return view('admin.post.edit', compact('tags', 'categories', 'post'));
     }
 
     /**
@@ -54,7 +94,41 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $this->validate($request,[
+            'title' => 'required',
+            'image' => 'image',
+            'categories' => 'required',
+            'tags' => 'required',
+            'body' => 'required'
+        ]);
+
+        // Upload Image
+        if(isset($request->image)){
+            //Image Upload
+            $imageName = time().'.'.$request->image->extension();
+            $request->image->move(public_path('storage/post'),$imageName);
+            $post->image = $imageName;
+        }
+
+        $post->user_id = Auth::id();
+        $post->title = $request->title;
+        $post->slug = Str::slug($request->title);
+        $post->image = $imageName;
+        $post->body = $request->body;
+        if(isset($request->status))
+        {
+            $post->status = true;
+        }else {
+            $post->status = false;
+        }
+        $post->is_approved = true;
+
+        $post->save();
+
+        $post->categories()->sync($request->categories);
+        $post->tags()->sync($request->tags);
+
+        return redirect()->route('post.index')->with('success', 'Post Updated Successfully');
     }
 
     /**
