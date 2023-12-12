602ec64e4777aca3e91d0a31dc91f96de94a027f
Kenneth Ossai <kenossai1@gmail.com>
2023-12-12 01:22:39 +0000

    Update SubscriberController.php

--- app/Http/Controllers/Admin/SubscriberController.php
+++ app/Http/Controllers/Admin/SubscriberController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Subscriber;
 use Illuminate\Http\Request;
 
 class SubscriberController extends Controller
 {
-    //
+    public function index()
+    {
+        $subscribers = Subscriber::latest()->get();
+        return view('admin.subscribers', compact('subscribers'));
+    }
+
+    public function destroy($subscriber)
+    {
+        $subscriber = Subscriber::findOrFail($subscriber);
+        $subscriber->delete();
+        return redirect()->back()->with('success', 'Subscriber deleted successfully');
+    }
 }
