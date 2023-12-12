0e2a4b339ba6c11d3bfaa1133a46c38e408e8da5
Kenneth Ossai <kenossai1@gmail.com>
2023-12-12 12:15:12 +0000

    Added notification function

--- app/Http/Controllers/Author/PostController.php
+++ app/Http/Controllers/Author/PostController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers\Author;
 
 use App\Models\Tag;
 use App\Models\Post;
+use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Notifications\NewAuthorPost;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\File;
 
-use function Laravel\Prompts\error;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Notification;
 
 class PostController extends Controller
 {
@@ -71,6 +73,9 @@ class PostController extends Controller
         $post->categories()->attach($request->categories);
         $post->tags()->attach($request->tags);
 
+        $users = User::where('role_id', '1')->get();
+        Notification::send($users, new NewAuthorPost($post));
+
         return redirect()->route('author.post.index')->with('success', 'Post Added Successfully');
     }
 
@@ -110,7 +115,7 @@ class PostController extends Controller
         {
             return redirect()->back()->with('error','You are not authorized to access this post');
         }
-        
+
         $this->validate($request,[
             'title' => 'required',
             'image' => 'image',
