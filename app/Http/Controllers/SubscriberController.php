58ca513e99e7c76cf90dcee1cc12cafa26c8cff2
Kenneth Ossai <kenossai1@gmail.com>
2023-12-12 00:58:27 +0000

    Added subscriber function

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subscriber;
 use Illuminate\Http\Request;
 
 class SubscriberController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email|unique:subscribers'
+        ]);
+
+        $subscriber = new Subscriber();
+        $subscriber->email = $request->email;
+        $subscriber->save();
+        return redirect()->back()->with('success', 'We have successfully added you to our subscriber list');
+    }
 }
