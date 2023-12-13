2704e73603832470052d74b7837bc7cb9cf5aa29
Kenneth Ossai <kenossai1@gmail.com>
2023-12-13 12:39:54 +0000

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $categories = Category::all();
+        $tags = Tag::all();
+        return view('welcome', compact('categories','tags'));
+    }
 }
