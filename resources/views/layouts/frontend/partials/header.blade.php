6a6e8383316ad6d6a76f9a8e25ff9e5d74f44c83
Kenneth Ossai <kenossai1@gmail.com>
2023-12-13 12:40:00 +0000

    Update header.blade.php

--- resources/views/layouts/frontend/partials/header.blade.php
+++ resources/views/layouts/frontend/partials/header.blade.php
@@ -45,46 +45,18 @@
           </div>
           <ul class="nav-menu ml-auto mr-auto" id="nav-menu-toggle">
             <li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Categories <span class="arrow-icon"> <span
+            <li class="nav-item"><a href="#" class="nav-link">Discussions <span class="arrow-icon"> <span
                     class="left-bar"></span>
                   <span class="right-bar"></span></span>
               </a>
               <ul class="drop-menu">
-                <li class="drop-menu-item"><a href="archive-layout-one.html">Food</a></li>
-                <li class="drop-menu-item"><a href="archive-layout-one.html">Technology</a></li>
-                <li class="drop-menu-item"><a href="archive-layout-one.html">Fashion</a></li>
-                <li class="drop-menu-item"><a href="archive-layout-one.html">Women</a></li>
-                <li class="drop-menu-item"><a href="archive-layout-one.html">Lifestyle</a></li>
+                @foreach ($categories as $category)
+                    <li class="drop-menu-item"><a href="archive-layout-one.html">{{ $category->name }}</a></li>
+                @endforeach
               </ul>
             </li>
-            <li class="nav-item drop-arrow"><a href="#" class="nav-link">Pages <span class="arrow-icon"> <span
-                    class="left-bar"></span>
-                  <span class="right-bar"></span></span></a>
-                  <ul class="drop-menu">
-                    <li class="drop-menu-item"><a href="index.html">Home Page One</a></li>
-                    <li class="drop-menu-item"><a href="index2.html">Home Page Two</a></li>
-                    <li class="drop-menu-item"><a href="index3.html">Home Page Three</a></li>
-                    <li class="drop-menu-item"><a href="index4.html">Home Page Dark</a></li>
-                    <li class="drop-menu-item"><a href="index5.html">Home Page RTL</a></li>
-                    <li class="drop-menu-item"><a href="404.html">404 Error Page One</a></li>
-                    <li class="drop-menu-item"><a href="404-text.html">404 Error Page Two</a></li>
-                    <li class="drop-menu-item"><a href="search.html">Search Page</a></li>
-                  </ul>
-            </li>
-            <li class="nav-item drop-arrow"><a href="#" class="nav-link">Layout <span class="arrow-icon"> <span
-                    class="left-bar"></span>
-                  <span class="right-bar"></span></span></a>
-              <ul class="drop-menu">
-                <li class="drop-menu-item"><a href="archive-layout-one.html">Archive Layout One</a></li>
-                <li class="drop-menu-item"><a href="archive-layout-two.html">Archive Layout Two</a></li>
-                <li class="drop-menu-item"><a href="archive-layout-three.html">Archive Layout Three</a></li>
-                <li class="drop-menu-item"><a href="archive-layout-four.html">Archive Layout Four</a></li>
-                <li class="drop-menu-item"><a href="single-layout-one.html">Single Layout One</a></li>
-                <li class="drop-menu-item"><a href="single-layout-two.html">Single Layout Two</a></li>
-                <li class="drop-menu-item"><a href="single-layout-three.html">Single Layout Three</a></li>
-                <li class="drop-menu-item"><a href="single-layout-four.html">Single Layout Four</a></li>
-              </ul>
-            </li>
+            <li class="nav-item drop-arrow"><a href="#" class="nav-link">Podcast</a></li>
+            <li class="nav-item drop-arrow"><a href="#" class="nav-link">Events</a></li>
             <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
             <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
           </ul>
