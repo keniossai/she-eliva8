65164dcafe7f989419094d34ae029bb85614a7ff
Kenneth Ossai <kenossai1@gmail.com>
2023-12-14 03:31:44 +0000

    Update 2023_12_14_031852_create_post_user_table.php

--- database/migrations/2023_12_14_031852_create_post_user_table.php
+++ database/migrations/2023_12_14_031852_create_post_user_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('post_user', function (Blueprint $table) {
             $table->id();
-            $table->integer('post_id')->unsigned();
+            $table->unsignedBigInteger('post_id')->unsigned();
             $table->integer('user_id');
             $table->foreign('post_id')
                 ->references('id')->on('posts')
