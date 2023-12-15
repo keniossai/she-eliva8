7e1497e572d44e0b46cdae9bf6b94c820fc5eac3
Kenneth Ossai <kenossai1@gmail.com>
2023-12-15 01:29:16 +0000

    Update FavoriteController.php

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FavoriteController extends Controller
 {
     public function makeFavorite($post)
     {
-        return $post;
+        $user  = Auth::user();
+        $isFavorite = $user->favorite_posts->where('post_id', $post)->count();
+
+        if($isFavorite == 0)
+        {
+            $user->favorite_posts()->attach($post);
+            return redirect()->back()->with('success', 'Post added as favorite');
+        } else{
+            $user->favorite_posts()->attach($post);
+            return redirect()->back()->with('success', 'Post removed from favorite');
+        }
     }
 }
