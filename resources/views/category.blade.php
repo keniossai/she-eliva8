897b7f154af3c4d9da3f330b458da10b24cea0f2
Kenneth Ossai <kenossai1@gmail.com>
2023-12-15 19:49:21 +0000

    Update category.blade.php

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -5,37 +5,72 @@
 
 
 @section('content')
-<section class="archive-content">
-    <div class="container">
-      <div class="row">
-        @foreach ($posts->category as $category)
-            <div class="col-md-4">
-                <div class="card simple-overlay-card border-0">
-                    <a href="single-layout-one.html"><img src="assets/images/town-street.jpg" class="card-img" alt=""></a>
-                    <div class="card-img-overlay">
-                    <ul class="category-tag-list">
-                        <li class="category-tag-name">
-                        <a href="#">Lifestyle</a>
-                        </li>
-                    </ul>
-                    <h5 class="card-title title-font">
-                        <a href="single-layout-one.html">10 ideas for travelling more</a>
-                    </h5>
-                    <div class="author-date">
-                        <a class="author" href="#">
-                        <img src="assets/images/person1.jpg" alt="" class="rounded-circle">
-                        <span class="writer-name-small">Sallie Allen
-                        </span>
-                        </a>
-                        <a class="date" href="#">
-                        <span>21 Dec, 2019</span>
-                        </a>
-                    </div>
-                    </div>
-                </div><!-- card -->
+<main class="kavya-archive kavya-archive-4">
+
+    <section class="page-header">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class=" col-md-6">
+              <div class="page-title">
+                <h2>{{ $category->name }}</h2>
+              </div>
             </div>
-        @endforeach
-      </div>
-    </div><!-- container close -->
-  </section>
+            <div class=" col-md-6">
+              <div class="page-breadcrumb">
+                <nav aria-label="breadcrumb">
+                  <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">{{ $category->name }}</li>
+                  </ol>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    <section class="archive-content">
+        <div class="container">
+          <div class="row">
+            @if ($category->posts->count() > 0)
+                @foreach ($category->posts as $post)
+                    <div class="col-md-4">
+                        <div class="card simple-overlay-card border-0">
+                            <a href="single-layout-one.html"><img src="{{ url('storage/post/'.$post->image) }}" class="card-img" alt=""></a>
+                            <div class="card-img-overlay">
+                            <ul class="category-tag-list">
+                                <li class="category-tag-name">
+                                <a href="#">{{ $category->name }}</a>
+                                </li>
+                            </ul>
+                            <h5 class="card-title title-font">
+                                <a href="single-layout-one.html">{{ $post->title }}</a>
+                            </h5>
+                            <div class="author-date">
+                                <a class="author" href="#">
+                                <img src="assets/images/person1.jpg" alt="" class="rounded-circle">
+                                <span class="writer-name-small">{{ $post->user->name }}
+                                </span>
+                                </a>
+                                <a class="date" href="#">
+                                <span>{{ $post->created_at->diffForHumans() }}</span>
+                                </a>
+                            </div>
+                            </div>
+                        </div><!-- card -->
+                    </div>
+                @endforeach
+            @else
+
+            @endif
+          </div>
+        </div><!-- container close -->
+      </section>
+      <section class="pagination-section">
+        <nav aria-label="Page navigation example">
+            <ul class="pagination justify-content-center">
+                {{-- {{ $category->posts->links() }} --}}
+            </ul>
+        </nav>
+    </section>
+</main>
 @endsection
