eef9c13c5e425a58f4d04c71e39cdf0a5e080ca1
Kenneth Ossai <kenossai1@gmail.com>
2023-12-15 14:23:47 +0000

    Created client posts controller

---
+++ app/Http/Controllers/AllPostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tag;
+use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class AllPostController extends Controller
+{
+    public function posts()
+    {
+        $categories = Category::all();
+        $tags = Tag::all();
+        $posts = Post::latest()->paginate(6);
+        return view('allposts', compact('categories','tags', 'posts'));
+    }
+}
