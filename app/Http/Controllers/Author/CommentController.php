05ab9111c9dce44ebde2d60a6d63ab69ef9cf0b0
Kenneth Ossai <kenossai1@gmail.com>
2023-12-15 17:29:57 +0000

    Update

---
+++ app/Http/Controllers/Author/CommentController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Author;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CommentController extends Controller
+{
+    public function index()
+    {
+        $comments = Comment::latest()->get();
+        return view('admin.comments.index', compact('comments'));
+    }
+
+    public function destroy($id)
+    {
+        Comment::findOrFail($id)->delete();
+        return redirect()->back()->with('success', 'Comment deleted successfully');
+    }
+}
