eddaf3318f1493c3093e5980dd2d2187c15ea1fa
Kenneth Ossai <kenossai1@gmail.com>
2023-12-17 17:39:09 +0000

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends Controller
         $categories = Category::take(3)->get();
         $tags = Tag::all();
         $posts = Post::latest()->approved()->published()->take(6)->get();
-        return view('welcome', compact('categories','tags', 'posts'));
+        $bannerposts = Post::approved()->published()->take(1)->inRandomOrder()->get();
+        return view('welcome', compact('categories','tags', 'posts', 'bannerposts'));
     }
 }
