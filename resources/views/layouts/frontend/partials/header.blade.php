8e7ec797482da457c9378a47a03d144aea27a224
Kenneth Ossai <kenossai1@gmail.com>
2023-12-31 17:55:26 +0000

    Update header.blade.php

--- resources/views/layouts/frontend/partials/header.blade.php
+++ resources/views/layouts/frontend/partials/header.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-3">
                 <div class="brand-name text-center">
                   <a href="{{ route('home') }}">
-                    <h1>Elivate-Her</h1>
+                    <h1>She Elevates</h1>
                     {{-- <span>Enter your tagline here</span> --}}
                   </a>
                 </div>
@@ -46,7 +46,7 @@
           </span>
           <div class="sticky-logo">
             <a href="{{ route('home') }}">
-              <p>Elivate-Her</p>
+              <p>She Elevates</p>
             </a>
           </div>
         <ul class="nav-menu ml-auto mr-auto" id="nav-menu-toggle">
@@ -100,140 +100,6 @@
       </div>
     </div>
     <!-- search overlay end -->
-
-    <!-- sticky sidebar -->
-    <div class="sticky-sidebar">
-      <div class="sticky-sidebar-content">
-        <div class="close-sidebar ml-auto">
-          <i class="fas fa-times"></i>
-        </div>
-        <h3>About the Author</h3>
-
-        <div class="author-img">
-          <img src="assets/images/author.jpg" alt="">
-        </div>
-        <div class="author-desc">
-          <h5 class="mb-2">Julie Ryan</h5>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat consequuntur vel quo, quae aliquam esse
-            facere eveniet magnam rerum! Quo itaque ipsa a ipsum quaerat optio illo ducimus dolores in!</p>
-        </div>
-        <div class="circular-icons social-links">
-          <ul>
-            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
-            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-
-        <div class="author-posts">
-          <h3>Most viewed</h3>
-          <div class="card mb-4">
-            <div class="row no-gutters">
-              <div class="col-4 col-md-4">
-                <a href="single-layout-one.html">
-                  <img src="assets/images/time.jpg" class="card-img" alt="">
-                </a>
-              </div>
-              <div class="col-8 col-md-8">
-                <div class="card-body">
-                  <ul class="category-tag-list">
-
-                    <li class="category-tag-name">
-                      <a href="#">Lifestyle</a>
-                    </li>
-                  </ul>
-                  <h5 class="card-title title-font"><a href="single-layout-one.html">Making time for travel</a>
-                  </h5>
-                  <div class="author-date">
-
-                    <a class="date" href="#">
-                      <span>21 Dec, 2019</span>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card mb-4">
-            <div class="row no-gutters">
-              <div class="col-4 col-md-4">
-                <a href="single-layout-one.html">
-                  <img src="assets/images/alone.jpg" class="card-img" alt="">
-                </a>
-              </div>
-              <div class="col-8 col-md-8">
-                <div class="card-body">
-                  <ul class="category-tag-list">
-                    <li class="category-tag-name">
-                      <a href="#">Lifestyle</a>
-                    </li>
-                  </ul>
-                  <h5 class="card-title title-font"><a href="single-layout-one.html">It's okay to be alone sometimes</a>
-                  </h5>
-                  <div class="author-date">
-                    <a class="date" href="#">
-                      <span>21 Dec, 2019</span>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card mb-4">
-            <div class="row no-gutters">
-              <div class="col-4 col-md-4">
-                <a href="single-layout-one.html">
-                  <img src="assets/images/forest.jpg" class="card-img" alt="">
-                </a>
-              </div>
-              <div class="col-8 col-md-8">
-                <div class="card-body">
-                  <ul class="category-tag-list">
-                    <li class="category-tag-name">
-                      <a href="#">travel</a>
-                    </li>
-                  </ul>
-                  <h5 class="card-title title-font"><a href="single-layout-one.html">Conserve Forest</a>
-                  </h5>
-                  <div class="author-date">
-                    <a class="date" href="#">
-                      <span>21 Dec, 2019</span>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card mb-4">
-            <div class="row no-gutters">
-              <div class="col-4 col-md-4">
-                <a href="single-layout-one.html">
-                  <img src="assets/images/beach-sea.jpg" class="card-img" alt="">
-                </a>
-              </div>
-              <div class="col-8 col-md-8">
-                <div class="card-body">
-                  <ul class="category-tag-list">
-                    <li class="category-tag-name">
-                      <a href="#">Lifestyle</a>
-                    </li>
-                  </ul>
-                  <h5 class="card-title title-font"><a href="single-layout-one.html">Beach is my favourite place</a>
-                  </h5>
-                  <div class="author-date">
-                    <a class="date" href="#">
-                      <span>21 Dec, 2019</span>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- sticky sidebar end -->
 
     <div class="body-overlay"></div>
   </header>
