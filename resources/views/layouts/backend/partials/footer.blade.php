e7c78d532be258342af758f46a6fc1b518035465
Kenneth Ossai <kenossai1@gmail.com>
2023-12-31 17:55:19 +0000

    Update footer.blade.php

--- resources/views/layouts/backend/partials/footer.blade.php
+++ resources/views/layouts/backend/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                &copy; <script>document.write(new Date().getFullYear())</script> She Elivate
+                &copy; <script>document.write(new Date().getFullYear())</script> She Elevates
             </div>
         </div>
     </div>
