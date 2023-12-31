0555601a7d0bec9e4bbca595b71ec91071a71cda
Kenneth Ossai <kenossai1@gmail.com>
2023-12-31 17:55:12 +0000

    Update verify-email.blade.php

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,31 +1,47 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-    </div>
+@extends('layouts.guest')
 
-    @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
-        </div>
-    @endif
+@section('title', 'Verify')
 
-    <div class="mt-4 flex items-center justify-between">
-        <form method="POST" action="{{ route('verification.send') }}">
-            @csrf
+@section('content')
+<div class="container">
+    <div class="row align-items-center justify-content-center">
+        <div class="col-md-8 col-lg-6 col-xl-5">
+            <div class="card">
 
-            <div>
-                <x-primary-button>
-                    {{ __('Resend Verification Email') }}
-                </x-primary-button>
+                <div class="card-body p-4">
+                    <div class="text-center mt-2">
+                        <h5 class="text-primary">Awesome!</h5>
+                        <div class="alert alert-success text-center mb-4" role="alert"> {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}</div>
+                    </div>
+                    <div class="p-2 mt-4">
+                        <form method="POST" action="{{ route('verification.send') }}">
+                            @csrf
+                            <div class="mt-3 text-center">
+                                <button class="btn btn-primary w-xl waves-effect waves-light" type="submit">{{ __('Resend Verification Email') }}</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="p-2 mt-2">
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <div class="mt-3 text-center">
+                                <button type="submit" class="btn btn-danger w-xl waves-effect waves-light">
+                                    {{ __('Log Out') }}
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
             </div>
-        </form>
 
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
+            <div class="mt-5 text-center">
+                <p> Copyrights &copy;<script>document.write(new Date().getFullYear())</script> Eliva8-Her</p>
+            </div>
 
-            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                {{ __('Log Out') }}
-            </button>
-        </form>
+        </div>
     </div>
-</x-guest-layout>
+    <!-- end row -->
+</div>
+@endsection
+
