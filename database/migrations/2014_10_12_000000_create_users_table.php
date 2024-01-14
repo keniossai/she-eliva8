2885fbda51f52bff4f5af900f14575bba13735b5
Kenneth Ossai <49806812+keniossai@users.noreply.github.com>
2024-01-14 00:46:43 +0000

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,18 +13,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->integer('role_id')->default(2);
             $table->string('name');
             $table->string('username')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('image')->default('default.png');
+            $table->enum('role',['admin','author','user'])->default('user');
             $table->text('about')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
