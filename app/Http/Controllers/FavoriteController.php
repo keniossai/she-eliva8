603303f77759de92ca976b2064eda0595f9ad458
Kenneth Ossai <49806812+keniossai@users.noreply.github.com>
2024-01-14 21:06:24 +0000

    Update FavoriteController.php

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -2,17 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class FavoriteController extends Controller
 {
     public function makeFavorite($post)
     {
-        $user  = Auth::user();
-        $isFavorite = $user->favorite_posts()->where('post_id',$post)->count();
+        $user = Auth::user();
+        $like = $user->favorite_posts()->where('post_id',$post)->count();
 
-        if ($isFavorite == 0)
+        if ($like == 0)
         {
             $user->favorite_posts()->attach($post);
             return redirect()->back()->with('success', 'Post added as favorite');
@@ -21,4 +23,23 @@ class FavoriteController extends Controller
             return redirect()->back()->with('success', 'Post removed from favorite');
         }
     }
+
+    // public function makeFavorite($post)
+    // {
+    //     $user = Auth::user();
+    //     $isFavorite = $user->favorite_posts()->where('post_id',$post)->count();
+
+    //     if($isFavorite == 0)
+    //     {
+    //         $user->favorite_posts()->attach($post);
+    //         return response()->json([
+    //             'message', 'You liked this story'
+    //         ], 200);
+    //     }else {
+    //         $user->favorite_posts()->detach($post);
+    //         return response()->json([
+    //             'message', 'You unliked this story'
+    //         ], 201);
+    //     }
+    // }
 }
