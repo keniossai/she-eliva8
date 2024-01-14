e3afa828e6dbcfb8b5ca6dd22ef9bef92ea6e193
Kenneth Ossai <49806812+keniossai@users.noreply.github.com>
2024-01-14 00:46:25 +0000

    Update AuthorMiddleware.php

--- app/Http/Middleware/AuthorMiddleware.php
+++ app/Http/Middleware/AuthorMiddleware.php
@@ -16,7 +16,7 @@ class AuthorMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if(Auth::check() && Auth::user()->role->id == 2)
+        if(Auth::check() && Auth::user()->role == "author")
         {
             return $next($request);
         } else {
