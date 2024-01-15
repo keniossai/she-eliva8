6385b54cd09bb805d71a8aa98470d893f02b2fcf
Kenneth Ossai <kenossai1@gmail.com>
2024-01-15 18:45:50 +0000

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,12 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import CounterComponent from './components/CounterComponent.vue'
+import PostCard from './components/PostCard.vue'
 
-import Alpine from 'alpinejs';
 
-window.Alpine = Alpine;
 
-Alpine.start();
+const app = createApp({});
+app.component('counter-component', CounterComponent);
+app.component('post-card', PostCard);
+
+app.mount('#app')
