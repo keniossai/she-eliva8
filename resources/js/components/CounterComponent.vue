eb7cdc19284e758dd8c1a48b6c644efeb1210f4e
Kenneth Ossai <kenossai1@gmail.com>
2024-01-15 18:45:45 +0000

    Create CounterComponent.vue

---
+++ resources/js/components/CounterComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {ref} from 'vue';
+const counter = ref(0);
+</script>
+<template>
+    <div>
+       number of times incremented: {{ counter }}
+    </div>
+    <button type="button" @click="counter++">increment</button>
+</template>
+
+
+{{  }}
