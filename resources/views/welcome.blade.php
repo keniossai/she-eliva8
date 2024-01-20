84f9ed0876580f9b51891abaaf6e976464a84c61
Kenneth Ossai <kenossai1@gmail.com>
2024-01-20 22:46:25 +0000

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,31 +71,7 @@
         <h2>Featured posts</h2>
       </div>
       <div class="row">
-        <div class="col-md-12 col-lg-7">
-          <div class="featured-left">
-            <div class="card">
-              <a href="{{ route('post.details',$post->slug) }}"><img src="client/images/image-8.jpg" class="card-img " alt="" /></a>
-              <div class="card-img-overlay">
-                <ul class="category-tag-list">
-
-                  <li class="category-tag-name">
-                    <a href="#">Fitness</a>
-                  </li>
-                </ul>
-                <h5 class="card-title title-font">
-                  <a href="#">Why Is It Important To Stay Fit</a>
-                </h5>
-                <p class="card-text mb-3">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-                  nostrum eum, cum ab iusto error totam quae nemo dicta nesciunt
-                  quo earum ipsam, ex aspernatur.
-                </p>
-                <a href="{{ route('post.details',$post->slug) }}" class="btn btn-solid btn-read">Read More</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-12 col-lg-5">
+        <div class="col-lg-6">
           <div class="featured-right">
             <div class="card">
               <a href="{{ route('post.details',$post->slug) }}">
@@ -121,21 +97,22 @@
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="featured-right">
             <div class="card">
               <a href="{{ route('post.details',$post->slug) }}">
-                <img src="client/images/image-9.jpg" class="card-img-top " alt="">
-
+                <img src="client/images/image-10.jpg" class="card-img-top " alt="">
               </a>
               <div class="card-body">
                 <ul class="category-tag-list">
+
                   <li class="category-tag-name">
                     <a href="#">Lifestyle</a>
                   </li>
-                  <li class="category-tag-name">
-                    <a href="#">Travel</a>
-                  </li>
                 </ul>
-                <h5 class="card-title title-font"><a href="{{ route('post.details',$post->slug) }}">Why College Students Love BasketBall</a>
+                <h5 class="card-title title-font"><a href="{{ route('post.details',$post->slug) }}">Exercises In Group For Motivation</a>
                 </h5>
                 <div class="author-date">
                   <a class="author" href="#">
