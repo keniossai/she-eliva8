c01eaf040eac160f5c415d3b05f1b4793101d16c
Kenneth Ossai <kenossai1@gmail.com>
2024-01-20 19:23:08 +0000

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,10 @@
 import './bootstrap';
 import { createApp } from 'vue';
-import CounterComponent from './components/CounterComponent.vue'
 import PostCard from './components/PostCard.vue'
 
 
 
 const app = createApp({});
-app.component('counter-component', CounterComponent);
 app.component('post-card', PostCard);
 
 app.mount('#app')
