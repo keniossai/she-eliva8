b131ecf0fe88105ee10a1f282503096c65238534
Kenneth Ossai <kenossai1@gmail.com>
2024-01-21 13:52:04 +0000

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,29 +13,29 @@
           </div>
           <div class="banner-bg-right">
             <div class="banner-carousel">
-                @foreach ($bannerposts as $post)
+                @foreach ($bannerPosts as $post)
                     <div class="banner-item">
 
-                    <div class="banner-img">
-                        <a href="{{ route('post.details',$post->slug) }}">
-                        <img src="{{ url('storage/post',$post->image) }}" alt="">
-                        </a>
-                    </div>
-
-                    <div class="banner-text">
-                        <ul class="category-tag-list">
-
-                        <li class="category-tag-name">
-                            <a href="#">{{ $post->category->name }}</a>
-                        </li>
-                        </ul>
-                        <div class="title-font">
-                        <a href="{{ route('post.details',$post->slug) }}">{{ $post->title }}</a>
+                        <div class="banner-img">
+                            <a href="{{ route('post.details',$post->slug) }}">
+                            <img src="{{ url('storage/post',$post->image) }}" alt="">
+                            </a>
                         </div>
-                        <p class="mb-3">{!! \Illuminate\Support\Str::limit($post->body,'200') !!}</p>
 
-                        <a href="{{ route('post.details',$post->slug) }}" class="btn btn-solid btn-read">Read More</a>
-                    </div>
+                        <div class="banner-text">
+                            <ul class="category-tag-list">
+
+                            <li class="category-tag-name">
+                                <a href="#">{{ $post->category->name }}</a>
+                            </li>
+                            </ul>
+                            <div class="title-font">
+                            <a href="{{ route('post.details',$post->slug) }}">{{ $post->title }}</a>
+                            </div>
+                            <p class="mb-3">{!! \Illuminate\Support\Str::limit($post->body,'200') !!}</p>
+
+                            <a href="{{ route('post.details',$post->slug) }}" class="btn btn-solid btn-read">Read More</a>
+                        </div>
                     </div>
                 @endforeach
             </div>
@@ -71,62 +71,19 @@
         <h2>Featured posts</h2>
       </div>
       <div class="row">
-        <div class="col-lg-6">
-          <div class="featured-right">
-            <div class="card">
-              <a href="{{ route('post.details',$post->slug) }}">
-                <img src="client/images/image-10.jpg" class="card-img-top " alt="">
-              </a>
-              <div class="card-body">
-                <ul class="category-tag-list">
-
-                  <li class="category-tag-name">
-                    <a href="#">Lifestyle</a>
-                  </li>
-                </ul>
-                <h5 class="card-title title-font"><a href="{{ route('post.details',$post->slug) }}">Exercises In Group For Motivation</a>
-                </h5>
-                <div class="author-date">
-                  <a class="author" href="#">
-                    <img src="client/images/writer.jpg" alt="" class="rounded-circle" />
-                    <span class="writer-name-small">Julie</span>
-                  </a>
-                  <a class="date" href="#">
-                    <span>21 Dec, 2019</span>
-                  </a>
-                </div>
-              </div>
+        @forelse ($featuredPosts as $post)
+            <div class="col-lg-6">
+                <feature-card
+                :post="{{ $post }}"
+                :created-at="'{{ $post->created_at->diffForHumans() }}'"
+                :is-guest="{{ (int) Auth::guest() }}"
+                :key="{{ $post->id }}"
+                ></feature-card>
             </div>
-          </div>
-        </div>
-        <div class="col-lg-6">
-          <div class="featured-right">
-            <div class="card">
-              <a href="{{ route('post.details',$post->slug) }}">
-                <img src="client/images/image-10.jpg" class="card-img-top " alt="">
-              </a>
-              <div class="card-body">
-                <ul class="category-tag-list">
+        @empty
 
-                  <li class="category-tag-name">
-                    <a href="#">Lifestyle</a>
-                  </li>
-                </ul>
-                <h5 class="card-title title-font"><a href="{{ route('post.details',$post->slug) }}">Exercises In Group For Motivation</a>
-                </h5>
-                <div class="author-date">
-                  <a class="author" href="#">
-                    <img src="client/images/writer.jpg" alt="" class="rounded-circle" />
-                    <span class="writer-name-small">Julie</span>
-                  </a>
-                  <a class="date" href="#">
-                    <span>21 Dec, 2019</span>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        @endforelse
+
       </div>
     </div>
   </section>
@@ -135,7 +92,7 @@
   <!-- Recommended Posts -->
   <section class="recommended-posts">
     <div class="container">
-      <div class="section-title pt-5">
+      <div class="section-title pt-2">
         <h2 class="pt-5">Recommended posts</h2>
       </div>
       <div class="more-content-grid ">
