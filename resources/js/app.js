074415f771d3ce7701d6b9565f616b395e8af9e2
Kenneth Ossai <kenossai1@gmail.com>
2024-01-21 00:14:48 +0000

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,12 @@
 import './bootstrap';
 import { createApp } from 'vue';
 import PostCard from './components/PostCard.vue'
+import FeatureCard from './components/FeatureCard.vue'
 
 
 
 const app = createApp({});
 app.component('post-card', PostCard);
+app.component('feature-card', FeatureCard);
 
 app.mount('#app')
