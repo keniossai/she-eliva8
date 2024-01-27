29d0b65422264326c734ae785800331181b4e700
Kenneth Ossai <kenossai1@gmail.com>
2024-01-27 19:41:25 +0000

    Update GoogleController.php

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
 
 class GoogleController extends Controller
 {
-    //
+    public function handleGoogleRedirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+    public function handleGoogleCallback()
+    {
+        try {
+           $user = Socialite::driver('google')->user();
+
+           $userExisted = User::where('oauth_id', $user->id)->where('oauth_type', 'google')->first();
+        } catch () {
+            //throw $th;
+        }
+    }
 }
