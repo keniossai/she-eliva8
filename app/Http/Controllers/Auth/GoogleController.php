02679b559829427758b1235801b876321a6ce1b8
Kenneth Ossai <kenossai1@gmail.com>
2024-01-27 20:33:10 +0000

    Update GoogleController.php

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Models\User;
 use Exception;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
 
 class GoogleController extends Controller
 {
+    CONST GOOGLE_TYPE = 'google';
+
     public function handleGoogleRedirect()
     {
         return Socialite::driver('google')->redirect();
@@ -20,8 +24,29 @@ class GoogleController extends Controller
            $user = Socialite::driver('google')->user();
 
            $userExisted = User::where('oauth_id', $user->id)->where('oauth_type', 'google')->first();
-        } catch () {
-            //throw $th;
+
+           if($userExisted){
+
+            Auth::login($userExisted);
+
+            return redirect()->route('home');
+
+           }else{
+            $newUser = User::create([
+                "name" => $user->name,
+                "email" => $user->email,
+                "oauth_id" => $user->id,
+                "oauth_type" => static::GOOGLE_TYPE,
+                "password" => Hash::make($user->id),
+            ]);
+
+            Auth::login($newUser);
+
+            return redirect()->route('home');
+
+           }
+        } catch (Exception $error) {
+            dd($error);
         }
     }
 }
