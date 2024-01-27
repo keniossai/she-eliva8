702a886a02d93bef5fbfc3aa175308ef8154435d
Kenneth Ossai <kenossai1@gmail.com>
2024-01-27 19:18:57 +0000

    Update FeatureCard.vue

--- resources/js/components/FeatureCard.vue
+++ resources/js/components/FeatureCard.vue
@@ -22,18 +22,18 @@ const addToFavorite = async (postId) => {
 </script> -->
 
 <template>
-    <div class="featured-right">
-        <div class="card">
+    <div class="">
+        <div class="card simple-overlay-card">
             <a :href="`/post/${post.slug}`">
-                <img :src="`/storage/post/${post.image}`" class="card-img-top " alt="" />
+                <img :src="`/storage/post/${post.image}`" class="card-img-top " alt="" style="height: 400px;" />
             </a>
-            <div class="card-body">
-            <ul class="category-tag-list">
+            <div class="card-img-overlay">
+            <ul class="category-tag-list d-flex">
                 <li class="category-tag-name">
-                    <a href="#">{{ post?.category?.name }}</a>
+                    <a href="#" class="text-white">{{ post?.category?.name }}</a>
                 </li>
                 <div class="comments_and_views">
-                    <li class="category-tag-name" >
+                    <li class="category-tag-name text-white">
                         <a v-if="isGuest" href="javascript:void(0)" class="likes space-x-1" onclick="toastr.info('You need to login to like a post','Info',{
                                 closeButton: true,
                                 progressBar: true,
@@ -44,8 +44,8 @@ const addToFavorite = async (postId) => {
                             <a v-else @click="addToFavorite(post.id)" style="background: none; border: none; color: rgb(247, 69, 69);" type="submit" class="likes space-x-1">
                                 <i class="fas fa-heart"></i> <span>{{ post?.favorite_to_user.length }}</span>
                             </a>
-                        </li>
-                        <li class="category-tag-name">
+                    </li>
+                    <li class="category-tag-name">
                         <a href="javascript:void(0)" class="view space-x-1" style="color: rgb(125, 128, 126);">
                             <i class="fas fa-eye"></i><span>{{ post.view_count }}</span>
                         </a>
@@ -62,18 +62,8 @@ const addToFavorite = async (postId) => {
                     {{ post.title }}
                 </a>
             </h5>
-            <div class="author-date">
-                <a class="author" href="#">
-                    <img :src="`/storage/profile/${post?.user?.image}`" alt="profile" class="rounded-circle" />
-                    <span class="writer-name-small">{{post?.user?.name}}</span>
-                </a>
-                <a class="date" href="javascript:void(0)">
-                    <span>{{ createdAt }}</span>
-                </a>
-            </div>
             </div>
         </div>
     </div>
-
 </template>
 
