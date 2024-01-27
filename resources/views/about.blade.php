7804bbdafd3d1efe025eb5aeab10feb2b18a3f80
Kenneth Ossai <kenossai1@gmail.com>
2024-01-27 19:19:12 +0000

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -238,92 +238,35 @@
       <div class="container">
         <h2>Meet our authors</h2>
         <div class="row">
-          <div class="col-md-6 col-lg-3">
-            <div class="card author-card">
-              <div class="card-img">
-                <img src="assets/images/writer.jpg" alt="">
-              </div>
-              <h5><a href="#">Julie Perry</a></h5>
-              <small>Chief author</small>
-              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsum labore quaerat magni
-                non saepe. Lorem ipsum dolor sit amet.</p>
 
-              <div class="social-links circular-icons">
-                <ul>
-                  <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                  <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                  <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-3">
-            <div class="card author-card">
-              <div class="card-img">
-                <img src="assets/images/author.jpg" alt="">
-              </div>
-              <h5><a href="#">Lidia Greig</a></h5>
-              <small>Chief author</small>
-              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsum labore quaerat magni
-                non saepe. Lorem ipsum dolor sit amet.</p>
 
-              <div class="social-links circular-icons">
-                <ul>
-                  <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                  <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                  <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-3">
-            <div class="card author-card">
-              <div class="card-img">
-                <img src="assets/images/person3.jpg" alt="">
-              </div>
-              <h5><a href="#">Ross Walton
-                </a></h5>
-              <small>Chief author</small>
-              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsum labore quaerat magni
-                non saepe. Lorem ipsum dolor sit amet.</p>
+          @foreach ($authors as $author)
+            <div class="col-md-6 col-lg-3">
+                <div class="card author-card">
+                <div class="card-img">
+                    <img src="storage/profile/{{ $author->image }}" alt="">
+                </div>
+                <h5><a href="#">{{ $author->name }}</a></h5>
+                <small>{{ $author->role }}</small>
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsum labore quaerat magni
+                    non saepe. Lorem ipsum dolor sit amet.</p>
 
-              <div class="social-links circular-icons">
-                <ul>
-                  <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                  <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                  <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                </ul>
-              </div>
+                <div class="social-links circular-icons">
+                    <ul>
+                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                    </ul>
+                </div>
+                </div>
             </div>
-          </div>
-          <div class="col-md-6 col-lg-3">
-            <div class="card author-card">
-              <div class="card-img">
-                <img src="assets/images/person2.jpg" alt="">
-              </div>
-              <h5><a href="#">Eden Shannon
-                </a></h5>
-              <small>Chief author</small>
-              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsum labore quaerat magni
-                non saepe. Lorem ipsum dolor sit amet.</p>
-              <div class="social-links circular-icons">
-                <ul>
-                  <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                  <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                  <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
+          @endforeach
         </div>
       </div>
     </section>
     <!-- About section -->
-   
+
     <!-- Scroll to top -->
     <div id="stop" class="scroll-to-top" style="opacity: 1;">
       <span><a href="#"><i class="fas fa-arrow-up"></i></a></span>
