e21c692536015217fff20a6a1c8753f2a7cfbac5
Kenneth Ossai <kenossai1@gmail.com>
2024-02-07 15:50:26 +0000

    Update app.blade.php

--- resources/views/layouts/backend/app.blade.php
+++ resources/views/layouts/backend/app.blade.php
@@ -31,9 +31,6 @@
 
 
     <body>
-
-    <!-- <body data-layout="horizontal" data-topbar="colored"> -->
-
         <!-- Begin page -->
         <div id="layout-wrapper">
 
@@ -104,16 +101,26 @@
         <!-- App js -->
         <script src="{{ asset('assets/js/app.js') }}"></script>
         <script src="{{ asset('assets/js/toastr.js') }}"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+        <script>
+            $('#summernote').summernote({
+            placeholder: 'Hello stand alone ui',
+            tabsize: 2,
+            height: 300,
+            toolbar: [
+                ['style', ['style']],
+                ['font', ['bold', 'underline', 'clear']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['table', ['table']],
+                ['insert', ['link', 'picture', 'video']],
+                ['view', ['fullscreen', 'codeview', 'help']]
+            ]
+            });
+        </script>
 
-        {{-- <script src="https://cdn.ckeditor.com/ckeditor5/40.1.0/classic/ckeditor.js"></script> --}}
-        {{-- <script src="{{ asset('assets/js/ckeditor.js') }}"></script> --}}
-        {{-- <script>
-            ClassicEditor
-                .create( document.querySelector( '#body' ) )
-                .catch( error => {
-                    console.error( error );
-                } );
-        </script> --}}
         {!! Toastr::message() !!}
         <script>
             @if($errors->any())
