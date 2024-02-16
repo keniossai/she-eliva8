7e5d34d58e93f351018e1e881dccbc5c2f94ed11
Kenneth Ossai <kenossai1@gmail.com>
2024-02-16 21:53:25 +0000

    Update app.blade.php

--- resources/views/layouts/backend/app.blade.php
+++ resources/views/layouts/backend/app.blade.php
@@ -3,179 +3,163 @@
 
 
 <!-- Mirrored from themesbrand.com/minible/layouts/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 07 Dec 2023 02:09:41 GMT -->
+
 <head>
 
-        <meta charset="utf-8" />
-        <title>@yield('title') - {{ config('app.name', 'Blog') }}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-        <meta content="Themesbrand" name="author" />
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="{{ asset('assets') }}/images/favicon.ico">
+    <meta charset="utf-8" />
+    <title>@yield('title') - {{ config('app.name', 'Blog') }}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets') }}/images/favicon.ico">
 
-        <!-- Bootstrap Css -->
-        <link href="{{ asset('assets') }}/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('assets') }}/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('assets') }}/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-        <!-- Icons Css -->
-        <link href="{{ asset('assets') }}/css/icons.min.css" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('assets/libs/select2/css/select2.min.css') }}" rel="stylesheet" type="text/css">
-        <link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css">
-        <!-- App Css-->
-        <link href="{{ asset('assets') }}/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('assets/css/toastr.css') }}" rel="stylesheet" />
-        <script src="https://kit.fontawesome.com/56626a3766.js" crossorigin="anonymous"></script>
-
-
-    </head>
-
-
-    <body>
-        <!-- Begin page -->
-        <div id="layout-wrapper">
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('assets') }}/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets') }}/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css"
+        rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets') }}/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet"
+        type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{ asset('assets') }}/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/libs/select2/css/select2.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css">
+    <!-- App Css-->
+    <link href="{{ asset('assets') }}/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/toastr.css') }}" rel="stylesheet" />
+    <script src="https://kit.fontawesome.com/56626a3766.js" crossorigin="anonymous"></script>
 
 
-            @include('layouts.backend.partials.header')
-            <!-- ========== Left Sidebar Start ========== -->
-            @include('layouts.backend.partials.sidebar')
-            <!-- Left Sidebar End -->
+</head>
+
+
+<body>
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+
+
+        @include('layouts.backend.partials.header')
+        <!-- ========== Left Sidebar Start ========== -->
+        @include('layouts.backend.partials.sidebar')
+        <!-- Left Sidebar End -->
 
 
 
-            <!-- ============================================================== -->
-            <!-- Start right Content here -->
-            <!-- ============================================================== -->
-            <div class="main-content">
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
 
-                <div class="page-content">
-                    @yield('content')
-                </div>
-                <!-- End Page-content -->
-
-
-                @include('layouts.backend.partials.footer')
+            <div class="page-content">
+                @yield('content')
             </div>
-            <!-- end main content-->
+            <!-- End Page-content -->
 
+
+            @include('layouts.backend.partials.footer')
         </div>
-        <!-- END layout-wrapper -->
+        <!-- end main content-->
+
+    </div>
+    <!-- END layout-wrapper -->
 
 
 
-        <!-- Right Sidebar -->
-        @include('layouts.backend.partials.rightside')
-        <!-- /Right-bar -->
+    <!-- Right Sidebar -->
+    @include('layouts.backend.partials.rightside')
+    <!-- /Right-bar -->
 
-        <!-- Right bar overlay-->
-        <div class="rightbar-overlay"></div>
+    <!-- Right bar overlay-->
+    <div class="rightbar-overlay"></div>
 
-        <!-- JAVASCRIPT -->
-        <script src="{{ asset('assets') }}/libs/jquery/jquery.min.js"></script>
-        <script src="{{ asset('assets') }}/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="{{ asset('assets') }}/libs/metismenu/metisMenu.min.js"></script>
-        <script src="{{ asset('assets') }}/libs/simplebar/simplebar.min.js"></script>
-        <script src="{{ asset('assets') }}/libs/node-waves/waves.min.js"></script>
-        <script src="{{ asset('assets') }}/libs/waypoints/lib/jquery.waypoints.min.js"></script>
-        <script src="{{ asset('assets') }}/libs/jquery.counterup/jquery.counterup.min.js"></script>
+    <!-- JAVASCRIPT -->
+    <script src="{{ asset('assets') }}/libs/jquery/jquery.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/metismenu/metisMenu.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/simplebar/simplebar.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/node-waves/waves.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/waypoints/lib/jquery.waypoints.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/jquery.counterup/jquery.counterup.min.js"></script>
 
-        <!-- apexcharts -->
-        <script src="{{ asset('assets/libs/apexcharts/apexcharts.min.js') }}"></script>
-        <script src="{{ asset('assets/libs/chart.js/Chart.bundle.min.js') }}"></script>
-        <script src="{{ asset('assets/js/pages/chartjs.init.js') }}"></script>
+    <!-- apexcharts -->
+    <script src="{{ asset('assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/chart.js/Chart.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/chartjs.init.js') }}"></script>
 
-        <script src="{{ asset('assets') }}/js/pages/dashboard.init.js"></script>
-        <!-- Required datatable js -->
-        <script src="{{ asset('assets') }}/libs/datatables.net/js/jquery.dataTables.min.js"></script>
-        <script src="{{ asset('assets') }}/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="{{ asset('assets') }}/js/pages/dashboard.init.js"></script>
+    <!-- Required datatable js -->
+    <script src="{{ asset('assets') }}/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
 
-        <!-- Responsive examples -->
-        <script src="{{ asset('assets') }}/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-        <script src="{{ asset('assets') }}/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+    <!-- Responsive examples -->
+    <script src="{{ asset('assets') }}/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
 
-        <!-- init js -->
-        <script src="{{ asset('assets') }}/js/pages/ecommerce-datatables.init.js"></script>
-        <script src="{{ asset('assets/libs/select2/js/select2.min.js') }}"></script>
-        <script src="{{ asset('assets/js/pages/ecommerce-add-product.init.js') }}"></script>
-        <script src="{{ asset('assets/js/pages/form-advanced.init.js') }}"></script>
+    <!-- init js -->
+    <script src="{{ asset('assets') }}/js/pages/ecommerce-datatables.init.js"></script>
+    <script src="{{ asset('assets/libs/select2/js/select2.min.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/ecommerce-add-product.init.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/form-advanced.init.js') }}"></script>
 
-        <!-- App js -->
-        <script src="{{ asset('assets/js/app.js') }}"></script>
-        <script src="{{ asset('assets/js/toastr.js') }}"></script>
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
-        <script>
-            $('#summernote').summernote({
-            placeholder: 'Hello stand alone ui',
-            tabsize: 2,
-            height: 300,
-            toolbar: [
-                ['style', ['style']],
-                ['font', ['bold', 'underline', 'clear']],
-                ['color', ['color']],
-                ['para', ['ul', 'ol', 'paragraph']],
-                ['table', ['table']],
-                ['insert', ['link', 'picture', 'video']],
-                ['view', ['fullscreen', 'codeview', 'help']]
-            ]
-            });
-        </script>
-
-        {!! Toastr::message() !!}
-        <script>
-            @if($errors->any())
-            @foreach($errors->all() as $error)
-            toastr.error('{{ $error }}','Error',{
-                closeButton:true,
-                progressBar:true,
-            });
-            @endforeach
-            @endif
-        </script>
-
-        <script>
-
-            // success message popup notification
-
-            @if(Session::has('success'))
-
-                toastr.success("{{ Session::get('success') }}");
-
-            @endif
+    <!-- App js -->
+    <script src="{{ asset('assets/js/app.js') }}"></script>
+    <script src="{{ asset('assets/js/toastr.js') }}"></script>
+    <script src="https://cdn.tiny.cloud/1/slug5mpr00nh39b5c8fcx4n036jq8ws5apjxymqhphby5e7o/tinymce/6/tinymce.min.js"
+        referrerpolicy="origin"></script>
+    <script>
+        tinymce.init({
+            selector: '#texteditor', // Replace this CSS selector to match the placeholder element for TinyMCE
+            plugins: [
+                'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
+                'searchreplace', 'wordcount', 'visualblocks', 'visualchars', 'code', 'fullscreen',
+                'insertdatetime',
+                'media', 'table', 'emoticons', 'template', 'help'
+            ],
+            toolbar: 'undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media | forecolor backcolor emoticons',
+        });
+    </script>
 
 
-            // info message popup notification
+    {!! Toastr::message() !!}
 
-            @if(Session::has('info'))
+    <script>
+        // success message popup notification
 
-                toastr.info("{{ Session::get('info') }}");
+        @if (Session::has('success'))
 
-            @endif
-
-
-            // warning message popup notification
-
-            @if(Session::has('warning'))
-
-                toastr.warning("{{ Session::get('warning') }}");
-
-            @endif
+            toastr.success("{{ Session::get('success') }}");
+        @endif
 
 
-            // error message popup notification
+        // info message popup notification
 
-            @if(Session::has('error'))
-                toastr.error("{{ Session::get('error') }}");
-            @endif
+        @if (Session::has('info'))
 
-            @foreach($errors->all() as $error)
-                toastr.error("{{ $error }}")
-            @endforeach
-        </script>
+            toastr.info("{{ Session::get('info') }}");
+        @endif
 
 
-    </body>
+        // warning message popup notification
+
+        @if (Session::has('warning'))
+
+            toastr.warning("{{ Session::get('warning') }}");
+        @endif
+
+
+        // error message popup notification
+
+        @if (Session::has('error'))
+            toastr.error("{{ Session::get('error') }}");
+        @endif
+
+        @foreach ($errors->all() as $error)
+            toastr.error("{{ $error }}")
+        @endforeach
+    </script>
+
+
+</body>
 
 
 </html>
-
