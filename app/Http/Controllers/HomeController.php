078fa78028bf39ea0c9b7c55557be051fe243887
Kenneth Ossai <kenossai1@gmail.com>
2024-02-17 11:53:37 +0000

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,7 @@ use App\Models\Role;
 use App\Models\User;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
@@ -15,7 +16,14 @@ class HomeController extends Controller
     {
         $categories = Category::take(3)->get();
         $tags = Tag::all();
-        $posts = Post::with(['category', 'favorite_to_user', 'user'])->latest()->approved()->published()->take(6)->get();
+        $posts = Cache::rememberForever('cached_posts', function () {
+            return Post::with(['category', 'favorite_to_user', 'user'])
+                ->latest()
+                ->approved()
+                ->published()
+                ->take(6)
+                ->get();
+        });
         $bannerPosts = Post::with(['category', 'favorite_to_user', 'user'])->approved()->published()->take(1)->inRandomOrder()->get();
         $recommendedPosts = Post::with(['category', 'favorite_to_user', 'user'])->approved()->published()->take(3)->get();
         $featuredPosts = Post::with(['category', 'favorite_to_user', 'user'])->approved()->published()->take(4)->get();
