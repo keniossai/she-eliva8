f75527831fe71dd0335a781fcd800fe7e7e45893
Kenneth Ossai <kenossai1@gmail.com>
2024-02-22 23:22:49 +0000

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,10 +9,11 @@ use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
 
 
